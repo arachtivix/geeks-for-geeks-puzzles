6fc17bbee23c610e4a9e5417a763af93167c7911
arachtivix <danielcswerner@gmail.com>
2018-03-31 14:45:22 -0400

    Create LargestNumberSumOfThreeOthersInSet.java
    This solution is probably exponential efficiency, so yeah...

---
+++ LargestNumberSumOfThreeOthersInSet.java
@@ -0,0 +1,48 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class LargestNumberSumOfThreeOthersInSet {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		List<Integer> in = new LinkedList<Integer>();
+		while(true){
+    		try{
+    		    in.add(sc.nextInt());
+    		} catch( NoSuchElementException e ){
+    		    break;
+    		}
+		}
+	    
+	    Collections.sort(in);
+	    Collections.reverse(in);
+		
+		Integer result = GFG.getLargestElementSumOfThreeOthersInSet(in);
+		System.out.println(result == null ? "No solution" : result);
+	}
+	
+	public static Integer getLargestElementSumOfThreeOthersInSet(List<Integer> in){
+	    
+	    int size = in.size();
+	    if(size < 4) return null;
+	    
+	    if( size == 4 ){
+	        int largest = in.remove(0);
+	        int sum = 0;
+	        for( Integer num : in ) sum += num;
+	        
+	        return sum == largest ? largest : null;
+	    } else {
+	        for(int i = 0; i < in.size(); i++){
+	            List<Integer> subset = new LinkedList<Integer>();
+	            subset.addAll(in);
+	            subset.remove(i);
+	            Integer result = GFG.getLargestElementSumOfThreeOthersInSet(subset);
+	            if( result != null ) return result;
+	        }
+	        return null;
+	    }
+	}
+}
